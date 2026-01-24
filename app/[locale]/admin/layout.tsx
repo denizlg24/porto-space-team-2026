79a92177b9cbbaf31d2ec1cde11c3a13e8bb39ed
denizlg24 <denizlg24@gmail.com>
2026-01-24 16:14:03 +0000

    fix(auth): disable prerender for admin

--- app/[locale]/admin/layout.tsx
+++ app/[locale]/admin/layout.tsx
@@ -5,19 +5,17 @@ import { AdminHeader } from "./_components/admin-header";
 import { auth } from "@/lib/auth";
 import { headers, cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { connection } from "next/server";
 
 const AdminLayout: NextLayoutIntlayer = async ({ children, params }) => {
+  await connection();
+
   const { locale } = await params;
   const cookieStore = await cookies();
 
-  const authCookie = cookieStore.get("porto_space_team.session_token");
-  console.log("LAYOUT AUTH COOKIE:", authCookie?.value ? "exists" : "missing");
-
   const session = await auth.api.getSession({ headers: await headers() });
-  console.log("LAYOUT SESSION:", session ? "found" : "not found");
 
   if (!session || session.user.approvalStatus !== "APPROVED") {
-    console.log("NO SESSION REDIRECTING TO SIGN IN");
     redirect(`/${locale}/sign-in`);
   }
   const sidebarState = cookieStore.get("sidebar_state");
