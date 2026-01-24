db572b366e4d4e0eb6b7f29f86c858db09e43881
denizlg24 <denizlg24@gmail.com>
2026-01-24 15:29:56 +0000

    fix(auth): trying to change optimistic redirect to use cookie instead of session

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,11 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 
 export const authProxy = async (req: NextRequest) => {
   if (!req.url.toLowerCase().includes("admin")) return NextResponse.next();
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+  const session = getSessionCookie(req,{
+    cookiePrefix:"porto_space_team",
+  })
 
   if (!session) {
     const localeCookie = req.cookies.get("INTLAYER_LOCALE")?.value ?? "en";
